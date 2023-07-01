507b55d57eebcd620d39d6b181d97530433f7535
rajalakshmibharath <rajalakshmibharath22@gmail.com>
2023-07-01 18:26:06 +0200

    training completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sign_language_prediction import logger
 from sign_language_prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from sign_language_prediction.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from sign_language_prediction.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 #we give stagfe name for all the satges
@@ -22,6 +23,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
