48da1a1ef5942f826ebb2a767919f5b4f1c3c4a5
rajalakshmibharath <rajalakshmibharath22@gmail.com>
2023-07-01 13:39:34 +0200

    Base model preparation completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sign_language_prediction import logger
 from sign_language_prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from sign_language_prediction.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 #we give stagfe name for all the satges
@@ -9,6 +10,18 @@ try:
 	 #we call the training pipeline and run the main
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
---
+++ src/sign_language_prediction/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,31 @@
+from sign_language_prediction.config.configuration import ConfigurationManager
+from sign_language_prediction.components.data_ingestion import DataIngestion
+from sign_language_prediction.components.prepare_base_model import PrepareBaseModel
+
+from sign_language_prediction import logger
+
+STAGE_NAME = "Prepare base model"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
